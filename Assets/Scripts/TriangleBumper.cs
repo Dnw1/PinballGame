79786301aff58def09b74ba2fdba4ba7178887d2
Dylan Waij <d_way@live.nl>
2015-10-31 01:16:41 +0100

    Update TriangleBumper.cs

--- Assets/Scripts/TriangleBumper.cs
+++ Assets/Scripts/TriangleBumper.cs
@@ -1,15 +1,13 @@
 using UnityEngine;
 using System.Collections;
+//Soufyan Hassan
 
 public class TriangleBumper : MonoBehaviour
 {
-<<<<<<< HEAD
 	public float force = 1000.0f;
 	public int scoreValue = 15;
-=======
     [SerializeField]
 	private float force = 1000.0f;
->>>>>>> origin/master
 	
 	void OnTriggerEnter(Collider col)
 	{
@@ -17,9 +15,6 @@ public class TriangleBumper : MonoBehaviour
 		{
 			col.GetComponent<Rigidbody>().AddForce(transform.forward * force);
 		}
-<<<<<<< HEAD
 		ScoreDisplay.score += scoreValue;
-=======
->>>>>>> origin/master
 	}
 }
